269680a310033d8ebc182417a1eb8c2ddd5c3194
Michael Kotliar <misha.kotliar@gmail.com>
2018-09-27 13:20:49 -0400

    Update upload function
    Not tested

--- biowardrobe_airflow_advanced/operators/deseq_jobgatherer.py
+++ biowardrobe_airflow_advanced/operators/deseq_jobgatherer.py
@@ -1,7 +1,6 @@
 import os
 from cwl_airflow_parser import CWLJobGatherer
 from biowardrobe_airflow_advanced.utils.upload import upload_deseq
-from biowardrobe_airflow_advanced.utils.analyze import get_deseq_job
 
 
 class DeseqJobGatherer(CWLJobGatherer):
@@ -11,7 +10,5 @@ class DeseqJobGatherer(CWLJobGatherer):
 
     def execute(self, context):
         job_result, promises = self.cwl_gather(context)
-        # conf = context['dag_run'].conf
-        # job = get_deseq_job(conf)
-        # upload_deseq(uid=job['uid'], filename=os.path.join(job["output_folder"], job["output_filename"]))
+        # upload_deseq(uid=promises['uid'], filename=os.path.join(promises["output_folder"], promises["output_filename"]))
         return job_result
--- biowardrobe_airflow_advanced/utils/upload.py
+++ biowardrobe_airflow_advanced/utils/upload.py
@@ -7,12 +7,11 @@ logger = logging.getLogger(__name__)
 
 
 def upload_deseq(uid, filename, clean=False):
-    logger.debug(f"Processing TSV file: {filename}")
+    logger.debug(f"Upload DESeq results from {filename}")
     connect_db = HookConnect()
     table_name = connect_db.get_settings["experimentsdb"] + '.`' + uid + '`'
-    logger.debug(f"Drop table {table_name}")
+    logger.debug(f"Drop table {table_name} if exists")
     connect_db.execute(f"DROP TABLE IF EXISTS {table_name}")
-    connect_db.execute(f"DROP VIEW IF EXISTS {table_name}")
     if not clean:
         logger.debug(f"Create table {table_name}")
         connect_db.execute(f"""CREATE TABLE {table_name}
@@ -22,13 +21,16 @@ def upload_deseq(uid, filename, clean=False):
                                  txStart INT NULL,
                                  txEnd INT NULL,
                                  strand VARCHAR(1),
-                                 TOT_R_0 FLOAT,
-                                 RPKM_0 FLOAT
+                                 uRpkm FLOAT,
+                                 tRpkm FLOAT,
+                                 LOGR FLOAT,
+                                 pvalue FLOAT,
+                                 padj FLOAT
                                 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 """)
-        SQL = f"INSERT INTO {table_name} (refseq_id,gene_id,chrom,txStart,txEnd,strand,TOT_R_0,RPKM_0) VALUES"
+        SQL = f"INSERT INTO {table_name} (refseq_id,gene_id,chrom,txStart,txEnd,strand,uRpkm,tRpkm,LOGR,pvalue,padj) VALUES"
         with open(filename, 'r') as input_file:
             for line in input_file.read().splitlines():
-                # RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand, TotalReads, Rpkm
+                # RefseqId, GeneId, Chrom, TxStart, TxEnd, Strand, uRpkm, tRpkm, log2FoldChange, pvalue, padj
                 if not line or "RefseqId" in line or "GeneId" in line:
                     continue
-                connect_db.execute(SQL + " (%s,%s,%s,%s,%s,%s,%s,%s)", tuple(line.split(',')))
+                connect_db.execute(SQL + " (%s,%s,%s,%s,%s,%s,%s,%s)".format(line.split()))
