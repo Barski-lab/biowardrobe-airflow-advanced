611e5c019032ee92e577aabb9311733e496740f4
Michael Kotliar <misha.kotliar@gmail.com>
2018-09-27 14:45:53 -0400

    Add script to run deseq

---
+++ biowardrobe_airflow_advanced/scripts/run_deseq.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+HOME=/home/airflow
+echo "/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf \"{\\\"condition\\\":[\\\"$1\\\", \\\"$2\\\"], \\\"groupby\\\":\\\"$3\\\", \\\"result_uid\\\":\\\"$4\\\"}\" deseq-advanced >/tmp/RunAdvanced.log 2>&1" >/tmp/RunAdvanced.log 2>&1
+/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf "{\"condition\":[\"$1\", \"$2\"], \"groupby\":\"$3\", \"result_uid\":\"$4\"}" deseq-advanced >>/tmp/RunAdvanced.log 2>&1
