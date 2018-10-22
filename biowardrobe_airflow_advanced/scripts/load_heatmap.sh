5250584eff3745bbbd7377a44df2a23d790670f7
Michael Kotliar <misha.kotliar@gmail.com>
2018-10-22 16:31:00 -0400

    Unimportant changes

--- biowardrobe_airflow_advanced/scripts/load_heatmap.sh
+++ biowardrobe_airflow_advanced/scripts/load_heatmap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 HOME=/home/airflow
-echo "/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap -u $1 2>&1 1 >> /tmp/RunAdvanced.log" >> /tmp/RunAdvanced.log 2>&1
-/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap -u $1 2>&1 1 >> /tmp/RunAdvanced.log
+echo "/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap --uid=$1 2>&1 1>>/tmp/RunAdvanced.log" >> /tmp/RunAdvanced.log 2>&1
+/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap --uid=$1 2>&1 1>>/tmp/RunAdvanced.log
