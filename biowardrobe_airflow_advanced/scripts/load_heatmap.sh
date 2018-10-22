de77da895988d0d08b2fcc2467b6b10f0196d82e
Michael Kotliar <misha.kotliar@gmail.com>
2018-10-22 17:28:14 -0400

    Remove unused script

--- biowardrobe_airflow_advanced/scripts/load_heatmap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-HOME=/home/airflow
-echo "/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap --uid=$1 2>&1 1>>/tmp/RunAdvanced.log" >> /tmp/RunAdvanced.log 2>&1
-/usr/bin/sudo -u airflow biowardrobe-advanced-load-heatmap --uid=$1 2>&1 1>>/tmp/RunAdvanced.log
