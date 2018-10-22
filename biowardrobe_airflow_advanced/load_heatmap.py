6edb2659116a4acf758a6d40f255c7d52882b483
Michael Kotliar <misha.kotliar@gmail.com>
2018-10-22 14:16:34 -0400

    Add --quiet option for heatmap uploader

--- biowardrobe_airflow_advanced/load_heatmap.py
+++ biowardrobe_airflow_advanced/load_heatmap.py
@@ -2,8 +2,12 @@
 import sys
 import logging
 import argparse
+from json import dumps
 
-from biowardrobe_airflow_advanced.utils.analyze import get_collected_heatmap_data
+from biowardrobe_airflow_advanced.utils.mute import Mute
+with Mute():  # Suppress output
+    from biowardrobe_airflow_advanced.utils.analyze import get_collected_heatmap_data
+    from biowardrobe_airflow_advanced.utils.logger import reset_root_logger
 
 
 logger = logging.getLogger(__name__)
@@ -12,6 +16,7 @@ logger = logging.getLogger(__name__)
 def get_parser():
     parser = argparse.ArgumentParser(description='BioWardrobe Airflow Advanced Load Heatmap', add_help=True)
     parser.add_argument("-u", "--uid", help="Genelist UID", required=True)
+    parser.add_argument("-q", "--quiet", action="store_true", help="Suppress all output except warnings and errors")
     return parser
 
 
@@ -19,7 +24,11 @@ def main(argsl=None):
     if argsl is None:
         argsl = sys.argv[1:]
     args,_ = get_parser().parse_known_args(argsl)
-    print(get_collected_heatmap_data(args.uid))
+
+    with Mute():
+        reset_root_logger(args.quiet)
+
+    print(dumps(get_collected_heatmap_data(args.uid)))
 
 
 if __name__ == "__main__":
