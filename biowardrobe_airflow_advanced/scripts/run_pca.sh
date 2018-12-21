97338b8b3aa6efe0d299f65e4f01edecf10548cf
Michael Kotliar <misha.kotliar@gmail.com>
2018-12-21 10:58:35 -0500

    Use bash in run_pca.sh

--- biowardrobe_airflow_advanced/scripts/run_pca.sh
+++ biowardrobe_airflow_advanced/scripts/run_pca.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 HOME=/home/airflow
 echo "/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf \"{\\\"uid\\\":\\\"$1\\\", \\\"expression\\\":\\\"${@:2}\\\"}\" pca >/tmp/RunAdvanced.log 2>&1" >> /tmp/RunAdvanced.log 2>&1
 /usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf "{\"uid\":\"$1\", \"expression\":\"${@:2}\"}" pca >> /tmp/RunAdvanced.log 2>&1
