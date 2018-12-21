e7506ba60c43f9fe8c66b022d3b1bbc88461df8c
Michael Kotliar <misha.kotliar@gmail.com>
2018-12-21 12:39:04 -0500

    Fix bug in run_pca.sh

--- biowardrobe_airflow_advanced/scripts/run_pca.sh
+++ biowardrobe_airflow_advanced/scripts/run_pca.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 HOME=/home/airflow
-echo "/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf \"{\\\"uid\\\":\\\"$1\\\", \\\"expression\\\":\\\"${@:2}\\\"}\" pca >/tmp/RunAdvanced.log 2>&1" >> /tmp/RunAdvanced.log 2>&1
-/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf "{\"uid\":\"$1\", \"expression\":\"${@:2}\"}" pca >> /tmp/RunAdvanced.log 2>&1
+E_UID=$1
+EXPR=${@:2}
+echo "/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf \"{\\\"uid\\\":\\\"$E_UID\\\", \\\"expression\\\":\\\"$EXPR\\\"}\" pca >/tmp/RunAdvanced.log 2>&1" >> /tmp/RunAdvanced.log 2>&1
+/usr/bin/sudo -u airflow /usr/local/bin/airflow trigger_dag --conf "{\"uid\":\"$E_UID\", \"expression\":\"$EXPR\"}" pca >> /tmp/RunAdvanced.log 2>&1
